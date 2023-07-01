d15b5f69548e2c7a21c0d0f2055a845712c32eeb
BlueTot <bluetotkc@gmail.com>
2023-07-01 22:25:28 +0100

    Update board.py

--- MVP/board.py
+++ MVP/board.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class Board:
 
     valid_nums = [str(i) for i in range(1, 10)]
@@ -5,6 +7,7 @@ class Board:
     def __init__(self):
         self.__size = 9
         self.__board = self.generate_new_board(self.__size)
+        self.__orig_board = deepcopy(self.__board)
 
     @staticmethod
     def generate_new_board(size=9):
@@ -16,3 +19,50 @@ class Board:
     
     def get_board(self):
         return self.__board
+    
+    @staticmethod
+    def __in_row(grid, row, num):
+        return num in grid[row]
+
+    @staticmethod
+    def __in_col(grid, col, num):
+        return num in [row[col] for row in grid]
+
+    @staticmethod
+    def __in_3x3_matrix(board, row, col, num):
+        curr_box = (row // 3, col // 3)
+        box = []
+        for row in range(3*curr_box[0], 3*(curr_box[0] + 1)):
+            for col in range(3*curr_box[1], 3*(curr_box[1]+1)):
+                box.append(board[row][col])
+        return num in box
+
+    def __is_safe(self, grid, row, col, num):
+        return (not self.__in_row(grid, row, num)) and (not self.__in_col(grid, col, num)) and (not self.__in_3x3_matrix(grid, row, col, num))
+
+    def __solve_sudoku(self, board, row=0, col=0):
+
+        if col == 9 and row == 8:
+            return True
+        
+        if col == 9:
+            col = 0
+            row += 1
+        
+        if board[row][col] > 0:
+            return self.__solve_sudoku(board, row, col+1)
+        for num in range(1, 10):
+            if self.__is_safe(board, row, col, num):
+                board[row][col] = num
+                if self.__solve_sudoku(board, row, col+1):
+                    return True
+            board[row][col] = 0
+
+        return False
+    
+    def get_solved_board(self):
+        new_board = deepcopy(self.__orig_board)
+        if self.__solve_sudoku(new_board):
+            return new_board
+        else:
+            return -1
