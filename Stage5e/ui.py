a2d0bcae6c64d8b85d070f3c1fb606a8fed6abd1
BlueTot <bluetotkc@gmail.com>
2023-10-09 12:30:20 +0100

    stage 5e files

---
+++ Stage5e/ui.py
@@ -0,0 +1,23 @@
+from stack import Stack
+from abc import ABC, abstractmethod
+
+class UI(ABC):
+
+    VERSION = "v0.5.4"
+
+    def __init__(self):
+        self._ui_stack = Stack()
+        self._push_ui_to_stack("home")
+    
+    def _push_ui_to_stack(self, ui):
+        self._ui_stack.push(ui)
+    
+    def _pop_ui_from_stack(self):
+        return self._ui_stack.pop()
+    
+    def _get_curr_ui(self):
+        return self._ui_stack.peek()
+
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError
